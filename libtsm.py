717592b542e91130f01f0a4b0a249aa32a4ff226
biagio <paparell@ins.uni-bonn.de>
2024-01-26 17:08:45 +0100

    First version of libsig is potentially ready.

---
+++ libtsm.py
@@ -0,0 +1,13 @@
+'''
+Time Series Manipulation library, suitable to manipulare time series
+before applying on them the signature transform.
+We have functions like log-return transforms, timestamp augmentation
+and zero-start injection.
+'''
+import torch
+
+
+
+
+if __name__ == "__main__":
+	print("Hello, world")
