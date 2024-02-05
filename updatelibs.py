fee1bbeed66e0b9384bdc73fb97feb5d87e2dac6
biagio <paparell@ins.uni-bonn.de>
2024-02-05 12:20:57 +0100

    Starding adding support for e new, revised version of sigwizard. Time Series Predictor.

--- updatelibs.py
+++ updatelibs.py
@@ -7,13 +7,13 @@ folder_dict = {}
 
 # Manually set the folders for each file
 folder_dict['libsig.py'] = ['../gmc/', '../up_or_down/']
-folder_dict['libsig.py'].append('../generative_simpletrain/')
+folder_dict['libsig.py'].append('../revised_sigwizard/')
 
 folder_dict['libtsm.py'] = ['../gmc/', '../up_or_down/']
-folder_dict['libtsm.py'].append('../generative_simpletrain/')
+folder_dict['libtsm.py'].append('../revised_sigwizard/')
 
 folder_dict['libmdl.py'] = ['../gmc/', '../up_or_down/']
-folder_dict['libmdl.py'].append('../generative_simpletrain/')
+folder_dict['libmdl.py'].append('../revised_sigwizard/')
 
 
 # Print a confirm message
